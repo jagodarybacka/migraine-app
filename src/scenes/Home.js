04700608897a51acd0c9496af72c2f067466c950
jagodarybacka <jagoda.roza.rybacka@gmail.com>
2018-05-24 19:21:37 +0200

    History Widget

--- src/scenes/Home.js
+++ src/scenes/Home.js
@@ -5,17 +5,23 @@ import {Link} from "react-router-dom";
 import Header from '../components/Header';
 import Menubar from '../components/Menubar';
 import Button from '../components/Button'
+import HistoryWidget from './history/HistoryWidget'
 
+const HomeComponent = styled.div`
+  margin-top: 6rem;
+  justify-content: normal;
+`
 
 const Home = () => {
   return (
-    <div className="Home">
+    <HomeComponent className="Home">
       <Header />
       <Link to="/add">
         <Button text="Add headache" />
       </Link>
+      <HistoryWidget />
       <Menubar />
-    </div>
+    </HomeComponent>
   );
 }
 
---
+++ src/scenes/history/HistoryWidget.js
@@ -0,0 +1,41 @@
+import React, {Component} from 'react';
+import styled from 'styled-components';
+import {Link} from "react-router-dom";
+
+import RecordCard from '../../components/RecordCard'
+
+
+const Widget = styled.section`
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  position: relative;
+
+  h3 {
+    text-transform: uppercase;
+    font-weight: 300;
+    margin: 1rem 0;
+    align-self: flex-start;
+  }
+  a {
+    margin: 1rem;
+    align-self: flex-end;
+    font-weight: 300;
+
+  }
+
+
+`
+const HistoryWidget = () => {
+  return (
+    <Widget >
+      <h3>Recent migraine</h3>
+      <RecordCard date="12 May" duration="3 hours" strength="Mild"/>
+      <Link to="#">See all...</Link>
+    </Widget>
+  );
+}
+
+
+export default HistoryWidget;
