833709e3208817723c4606dcbd182721220b876f
zwojtyniak <zuzwoj2@st.amu.edu.pl>
2018-06-22 22:51:12 +0200

    Add axios default settings

--- src/index.js
+++ src/index.js
@@ -4,5 +4,8 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
+axios.defaults.baseURL = 'http://localhost:5100';
+axios.defaults.withCredentials = true;
+
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
