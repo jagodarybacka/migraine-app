305cebd630742b0aa495bdff535f86a674f76fe0
zuzannaszymanda <32640873+zuzannaszymanda@users.noreply.github.com>
2019-01-05 21:05:00 +0100

    add route

--- src/client/base/mobile/Mobile.js
+++ src/client/base/mobile/Mobile.js
@@ -43,6 +43,7 @@ const Mobile = () => {
           <Route path="/reset/:token" component={ResetPassword}/>
           <PrivateRoute exact isLogged={isLogged} path="/home" component={Home} />
           <PrivateRoute exact isLogged={isLogged} path="/add" component={RecordForm} />
+          <PrivateRoute exact isLogged={isLogged} path="/edit/:id/" component={RecordForm} />
           <PrivateRoute exact isLogged={isLogged} path="/history" component={History} />
           <PrivateRoute exact isLogged={isLogged} path="/settings" component={Settings} />
           <PrivateRoute exact isLogged={isLogged} path="/summary" component={Summary} />
