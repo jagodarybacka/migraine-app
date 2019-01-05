083f5d6623f8d5f45003e593c4bb5d6463e968a6
zuzannaszymanda <32640873+zuzannaszymanda@users.noreply.github.com>
2019-01-05 18:08:47 +0100

    cleanup

--- src/server/handlers/errorHandlers.js
+++ src/server/handlers/errorHandlers.js
@@ -27,16 +27,3 @@ exports.notFound = (req, res, next) => {
 	err.status = 404;
 	next(err);
 };
-
-/*
-  MongoDB Validation Error Handler
-  Detect if there are mongodb validation errors that we can nicely show via flash messages
-*/
-
-// exports.flashValidationErrors = (err, req, res, next) => {
-// 	if (!err.errors) return next(err);
-// 	// validation errors look like
-// 	const errorKeys = Object.keys(err.errors);
-// 	errorKeys.forEach(key => req.flash("error", err.errors[key].message));
-// 	console.log(res);
-// };
