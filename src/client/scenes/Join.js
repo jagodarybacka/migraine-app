18276484d73eb5c6945a1ff62e42a040b28fdfe5
zuzannaszymanda <32640873+zuzannaszymanda@users.noreply.github.com>
2019-01-20 15:46:34 +0100

    change language

--- src/client/scenes/Join.js
+++ src/client/scenes/Join.js
@@ -1,10 +1,29 @@
 import React, { Component } from 'react';
+import styled from 'styled-components';
 import {Link} from "react-router-dom";
-import {languageText} from '../languages/MultiLanguage.js';
+import {languageText, setLanguage} from '../languages/MultiLanguage.js';
+import pl from '../assets/lang/plIcon.png';
+import eng from '../assets/lang/engIcon.png';
 
 import Logo from '../components/Logo'
 import Button from '../components/Button'
 
+const Language = styled.div`
+  position: absolute;
+  top: 0;
+  right: 0;
+  display: flex;
+  flex-direction: row;
+  justify-content: flex-end;
+  margin: 0 2vw 0 2vw;
+
+  .language__button {
+    margin: 0 0.5em;
+    outline: none;
+    border: none;
+    padding: 0;
+  }
+`
 class Join extends Component {
   constructor(props) {
     super(props);
@@ -12,17 +31,20 @@ class Join extends Component {
     if (window.localStorage.getItem('isLogged') === 'true') {
       props.history.push('/home');
     }
-
-    this.loginFacebook = this.loginFacebook.bind(this);
   }
 
-  loginFacebook() {
-    console.log('facebook');
+  setNewLanguage(lang) {
+    setLanguage(lang);
+    window.location.reload();
   }
 
   render() {
     return (
       <div>
+        <Language>
+            <img className="language__button" src={pl} alt={languageText.web.plAlt} onClick={() => this.setNewLanguage('pl')} />
+            <img className="language__button" src={eng} alt={languageText.web.engAlt} onClick={() => this.setNewLanguage('eng')} />
+        </Language>
         <h1 style={{textTransform: 'uppercase'}}>{languageText.join.migraine}</h1>
         <Logo size='80px' margin='5% 0'/>
         <Link to="/login">
