a5fdcd889897a0a931a28c535eb941fe6049f7ac
zuzannaszymanda <32640873+zuzannaszymanda@users.noreply.github.com>
2019-01-20 13:07:46 +0100

    language settings

--- src/client/languages/MultiLanguage.js
+++ src/client/languages/MultiLanguage.js
@@ -17,8 +17,21 @@ export let languageText =(()=>{
     if(window.langFile === undefined){
         let language = localStorage.getItem('lang');
         if(language == null){
-            localStorage.setItem('lang', 'eng');
-            language = 'eng';
+            var userLang = navigator.language || navigator.userLanguage || navigator.browserLanguage; 
+            console.log(userLang);
+            try {
+                userLang = userLang.split('-');
+                if(userLang[0] === 'pl' || userLang[0] === 'eng')
+                    setLanguage(userLang[0])
+                else 
+                    setLanguage('eng');
+            } catch {
+                if(userLang === 'pl' || userLang === 'eng')
+                    setLanguage(userLang);
+                else 
+                    setLanguage('eng');
+            }
+            language = localStorage.getItem('lang');
         }
         let json = require('../languages/'+language+'.json');
         window.langFile = json;
