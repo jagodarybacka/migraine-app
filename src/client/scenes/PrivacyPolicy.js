4a8ceccce49ffc180c34852cad2c249bf7e77731
zwojtyniak <zuzwoj2@st.amu.edu.pl>
2019-01-29 02:50:38 +0100

    Added Privacy Police

--- src/client/scenes/PrivacyPolicy.js
+++ src/client/scenes/PrivacyPolicy.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import styled from 'styled-components'
 import { Link } from 'react-router-dom'
-//import { languageText } from '../../languages/MultiLanguage';
+import { languageText } from '../languages/MultiLanguage';
 import closeIcon from '../assets/exit.png'
 import closeIconWhite from '../assets/exit-white.png'
 import {getTheme} from '../themes/ThemeHandler.js';
@@ -17,7 +17,7 @@ const ExitIcon = styled.img`
   opacity: 0.7;
 `
 
-const PrivacyPolicy2 =  styled.p`
+const PrivacyPolicy2 =  styled.div`
   font-weight: 300;
   justify-content: left;
   padding: 0 5%;
@@ -32,8 +32,63 @@ const PrivacyPolicy = () => {
           <Link to="/register" >
             <ExitIcon src={getTheme()=="DarkTheme" ? closeIconWhite : closeIcon} /> 
           </Link>
-            <p>Privacy Policy </p>
-        </PrivacyPolicy2>
+          <h1>{languageText.privacyPolicy.privacyPolicy2}</h1>
+
+          <p>{languageText.privacyPolicy.generalParagraph1}</p>
+
+          <p>{languageText.privacyPolicy.generalParagraph2}</p>
+
+          <h2>{languageText.privacyPolicy.informationCollection}</h2>
+
+            <p>{languageText.privacyPolicy.informationCollectionParagraph}</p>
+
+            <h3>{languageText.privacyPolicy.types}</h3>
+
+            <li>{languageText.privacyPolicy.personal}</li>
+              <p>{languageText.privacyPolicy.personalParagraph}</p>
+
+            <li>{languageText.privacyPolicy.record}</li>
+              <p>{languageText.privacyPolicy.recordParagraph}</p>
+
+            <li>{languageText.privacyPolicy.location}</li>
+             <p>{languageText.privacyPolicy.locationParagraph}</p>
+
+          <h2>{languageText.privacyPolicy.disclosure}</h2>
+
+            <h3>{languageText.privacyPolicy.legal}</h3>
+            <p>{languageText.privacyPolicy.legalParagraph}</p>
+            <ul>
+              <li>{languageText.privacyPolicy.legalLi1}</li>
+              <li>{languageText.privacyPolicy.legalLi2}</li>
+              <li>{languageText.privacyPolicy.legalLi3}</li>
+              <li>{languageText.privacyPolicy.legalLi4}</li>
+              <li>{languageText.privacyPolicy.legalLi5}</li>
+            </ul>
+
+
+          <h2>{languageText.privacyPolicy.security}</h2>
+            <p>{languageText.privacyPolicy.securityParagraph}</p>
+
+          <h2>{languageText.privacyPolicy.use}</h2>
+            <p>{languageText.privacyPolicy.useParagraph}</p>
+
+          <h2>{languageText.privacyPolicy.modification}</h2>
+            <p>{languageText.privacyPolicy.modificationParagraph}</p>
+
+          <h2>{languageText.privacyPolicy.children}</h2>
+            <p>{languageText.privacyPolicy.childrenParagraph}</p>
+
+          <h2>{languageText.privacyPolicy.note}</h2>
+            <p>{languageText.privacyPolicy.noteParagraph}</p>
+
+
+          <h2>{languageText.privacyPolicy.contact}</h2>
+            <p>{languageText.privacyPolicy.contactParagraph}</p>
+            <ul>
+              <li>E-mail: migraine.app@vp.pl</li>       
+            </ul>  
+
+         </PrivacyPolicy2>
     )
 }
 
